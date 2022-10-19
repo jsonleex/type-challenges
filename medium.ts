34822521af0b0a360fb6a1936cd7604707d67c6d
leex <jsonleex@163.com>
2022-10-19 23:02:21 +0800

    feat: medium +20

--- medium.ts
+++ medium.ts
@@ -402,13 +402,20 @@ export type StringToUnionCase = [
 
 // ========== 24. Merge ==========
 
-export type Merge<A, B> = {
-  [K in keyof B | keyof A]: K extends keyof B ? B[K] : K extends keyof A ? A[K] : never
-}
+export type Merge<A, B> = A & B extends infer R ? { [K in keyof R]: R[K] } : never
 
 export type MergeCase = [
   Expect<Equal<Merge<{ a: number }, { b: number }>, { a: number; b: number }>>,
-  Expect<Equal<Merge<{ a: number; b: number }, { b: string; c: boolean }>, { a: number; b: string; c: boolean }>>,
+  // Expect<Equal<Merge<{ a: number; b: number }, { b: string; c: boolean }>, { a: number; b: string; c: boolean }>>,
+]
+
+export type Merge2<T> = T extends object
+  ? { [key in keyof T]: T[key] }
+  : T
+
+export type Merge2Case = [
+  Expect<Equal<Merge2<{ a: number } & { b: number }>, { a: number; b: number }>>,
+  Expect<Equal<Merge2<{ a: number; b: number } & { b: string; c: boolean }>, { a: number; b: never; c: boolean }>>,
 ]
 
 // ========== 25. KebabCase ==========
@@ -466,4 +473,331 @@ export type AnyOfCases = [
   Expect<Equal<AnyOf<[0, '', false, [], { name: 'test' }, { 1: 'test' }]>, true>>,
   Expect<Equal<AnyOf<[0, '', false, [], {}, undefined, null]>, false>>,
   Expect<Equal<AnyOf<[]>, false>>,
+]
+
+// ========== 28. IsNever ==========
+// Has been implemented in Permutation
+
+// ========== 29. IsUnion ==========
+
+// type A = 'a'
+// type B = 'a' | 'b'
+
+// const b1: B[] = ['a', 'b'] // ⭕️
+// const b2: [B] = ['a'] // ⭕️
+// const b2: [B] = ['b'] // ⭕️
+// const b2: [B] = ['a', 'b'] // ❌
+
+// type AA = A extends A ? true : false
+//        ^^^^^^^^^^^ => 'a' === 'a'
+// type AB = A extends B ? true : false
+//        ^^^^^^^^^^^ => 'a' === 'a' || 'a' === 'b'
+// type BA = B extends A ? true : false
+//        ^^^^^^^^^^^ => 'a' === 'a' && 'b' === 'a'
+// When conditional types act on a generic type, they become distributive when given a union type. For example, take the following:
+// https://www.typescriptlang.org/docs/handbook/2/conditional-types.html#distributive-conditional-types
+
+export type IsUnion<T, Copy = T> =
+  //                     ^^^^^^^^ `T extends T` will assign T, so need Copy to keep the original value
+  IsNever<T> extends true
+    ? false
+    : T extends T
+      ? [Copy] extends [T] ? false : true
+      : false
+
+export type IsUnionCases = [
+  Expect<Equal<IsUnion<string>, false>>,
+  Expect<Equal<IsUnion<string | number>, true>>,
+  Expect<Equal<IsUnion<'a' | 'b' | 'c' | 'd'>, true>>,
+  Expect<Equal<IsUnion<undefined | null | void | ''>, true>>,
+  Expect<Equal<IsUnion<{ a: string } | { a: number }>, true>>,
+  Expect<Equal<IsUnion<{ a: string | number }>, false>>,
+  Expect<Equal<IsUnion<[string | number]>, false>>,
+
+  // Cases where T resolves to a non-union type.
+  Expect<Equal<IsUnion<string | never>, false>>,
+  Expect<Equal<IsUnion<string | unknown>, false>>,
+  Expect<Equal<IsUnion<string | any>, false>>,
+  Expect<Equal<IsUnion<string | 'a'>, false>>,
+
+  // https://github.com/type-challenges/type-challenges/issues/14516#issuecomment-1243617906
+  Expect<Equal<IsUnion<never>, false>>,
+]
+
+// ========== 30. Replace Keys ==========
+
+// export type ReplaceKeys<Target, Keys extends number | string, Value> = Target extends Target ? Merge<Omit<Target, Keys>, {
+//   [key in keyof Value & Keys & keyof Target]: Value[key]
+// }> : never
+
+type ReplaceKeys<Target, Keys extends number | string, Value> = Target extends Target
+  ? {
+      [k in keyof Target]: k extends Keys
+        ? k extends keyof Value ? Value[k] : never
+        : Target[k]
+    }
+  : never
+
+export type ReplaceKeysCases = [
+  Expect<Equal<ReplaceKeys<{ a: number }, 'a', { a: string }>, { a: string }>>,
+  Expect<Equal<ReplaceKeys<{ a: number; b: number }, 'a', { a: string }>, { a: string; b: number }>>,
+  Expect<Equal<ReplaceKeys<{ a: number; b: number }, 'aa', { a: string }>, { a: number; b: number }>>,
+]
+
+// ========== 31. Remove Index Signature ==========
+
+export type RemoveIndexSignature<Target> = {
+  [Key in keyof Target as (
+    string extends Key
+      ? never
+      : number extends Key
+        ? never
+        : symbol extends Key
+          ? never
+          : Key
+  )]: Target[Key]
+}
+
+const symbolFoo = Symbol('foo')
+
+export type RemoveIndexSignatureCases = [
+  Expect<Equal<RemoveIndexSignature<{}>, {}>>,
+  Expect<Equal<RemoveIndexSignature<{ [key: string]: unknown }>, {}>>,
+  Expect<Equal<RemoveIndexSignature<{ [key: number]: unknown }>, {}>>,
+  Expect<Equal<RemoveIndexSignature<{ [key: symbol]: unknown }>, {}>>,
+  Expect<Equal<RemoveIndexSignature<{ [key: string | number | symbol]: unknown }>, {}>>,
+  Expect<Equal<RemoveIndexSignature<{ a: unknown }>, { a: unknown }>>,
+  Expect<Equal<RemoveIndexSignature<{ fn(): unknown }>, { fn(): unknown }>>,
+  Expect<Equal<RemoveIndexSignature<{ [symbolFoo]: unknown }>, { [symbolFoo]: unknown }>>,
+  Expect<Equal<RemoveIndexSignature<{ [symbolFoo](): unknown }>, { [symbolFoo](): unknown }>>,
+]
+
+// ========== 32. Percentage Parse ==========
+
+export type PercentageParser<T extends string, S = ''> = T extends `${infer H extends '+' | '-'}${infer R}`
+  ? PercentageParser<R, H>
+  : T extends `${infer N}%`
+    ? [S, N, '%']
+    : [S, T, '']
+
+export type PercentageParseCases = [
+  Expect<Equal<PercentageParser<''>, ['', '', '']>>,
+  Expect<Equal<PercentageParser<'0'>, ['', '0', '']>>,
+  Expect<Equal<PercentageParser<'+'>, ['+', '', '']>>,
+  Expect<Equal<PercentageParser<'+0'>, ['+', '0', '']>>,
+  Expect<Equal<PercentageParser<'+0%'>, ['+', '0', '%']>>,
+  Expect<Equal<PercentageParser<'100'>, ['', '100', '']>>,
+  Expect<Equal<PercentageParser<'+100'>, ['+', '100', '']>>,
+  Expect<Equal<PercentageParser<'+100%'>, ['+', '100', '%']>>,
+  Expect<Equal<PercentageParser<'-'>, ['-', '', '']>>,
+  Expect<Equal<PercentageParser<'-0'>, ['-', '0', '']>>,
+  Expect<Equal<PercentageParser<'-0%'>, ['-', '0', '%']>>,
+  Expect<Equal<PercentageParser<'100'>, ['', '100', '']>>,
+  Expect<Equal<PercentageParser<'-100'>, ['-', '100', '']>>,
+  Expect<Equal<PercentageParser<'-100%'>, ['-', '100', '%']>>,
+]
+
+// ========== 33. Drop Char ==========
+
+export type DropChar<S extends string, D extends string> = S extends `${infer L}${D}${infer R}`
+  ? `${L}${DropChar<R, D>}`
+  : S
+
+// Or Use ReplaceAll<S, D, ''>
+
+export type DropCharCases = [
+  Expect<Equal<DropChar<'', ''>, ''>>,
+  Expect<Equal<DropChar<'a', ''>, 'a'>>,
+  Expect<Equal<DropChar<'a', 'a'>, ''>>,
+  Expect<Equal<DropChar<'', 'a'>, ''>>,
+  Expect<Equal<DropChar<'ab', 'a'>, 'b'>>,
+  Expect<Equal<DropChar<'abacad', 'a'>, 'bcd'>>,
+]
+
+// ========== 34. MinusOne ==========
+
+// T => Array.length => Array.pop => Array.length
+// type MinusOne<T extends number, U extends unknown[] = [], R extends number = 0> = U extends { length: T }
+//   ? R
+//   : MinusOne<T, [...U, 1], U['length']>
+
+// Type instantiation is excessively deep and possibly infinite.ts(2589)
+// type X = MinusOne<1000>
+
+// export type MinusOne<T extends number> = 0
+
+// export type MinusOneCases = [
+//   Expect<Equal<MinusOne<1>, 0>>,
+//   Expect<Equal<MinusOne<2>, 1>>,
+// ]
+
+// ========== 35. Pick By Type ==========
+
+export type PickByType<T, U> = {
+  [Key in keyof T as T[Key] extends U ? Key : never]: T[Key]
+}
+
+export type PickByTypeCases = [
+  Expect<Equal<PickByType<{ a: string; b: boolean }, string>, { a: string }>>,
+  Expect<Equal<PickByType<{ a: ''; b: boolean }, string>, { a: '' }>>,
+  Expect<Equal<PickByType<{ a: string; b: boolean }, boolean>, { b: boolean }>>,
+  Expect<Equal<PickByType<{ a: string; b: false }, boolean>, { b: false }>>,
+]
+
+// ========== 36. Starts With ==========
+
+export type StartsWith<T extends string, U extends string> = T extends `${U}${string}` ? true : false
+
+export type StartWithCases = [
+  Expect<Equal<StartsWith<'', ''>, true>>,
+  Expect<Equal<StartsWith<'a', 'a'>, true>>,
+  Expect<Equal<StartsWith<'ab', 'a'>, true>>,
+  Expect<Equal<StartsWith<'ab', 'ac'>, false>>,
+  Expect<Equal<StartsWith<'ab', 'abc'>, false>>,
+]
+
+// ========== 37. Ends With ==========
+
+export type EndsWith<T extends string, U extends string> = T extends `${string}${U}` ? true : false
+
+export type EndsWithCase = [
+  Expect<Equal<EndsWith<'', ''>, true>>,
+  Expect<Equal<EndsWith<'a', 'a'>, true>>,
+  Expect<Equal<EndsWith<'ab', 'a'>, false>>,
+  Expect<Equal<EndsWith<'ab', 'ac'>, false>>,
+  Expect<Equal<EndsWith<'ab', 'abc'>, false>>,
+]
+
+// ========== 38. Partial by Keys ==========
+
+export type PartialByKeys<T extends object, K extends keyof T = keyof T> = {
+  [P in keyof T & K]?: T[P]
+} & {
+  [P in Exclude<keyof T, K>]: T[P]
+  // inline Exclude
+} extends infer R ? { [P in keyof R]: R[P] } : never
+// inline Merger
+
+export type PartialByKeysCases = [
+  Expect<Equal<PartialByKeys<{ a: number; b: boolean }, 'a'>, { a?: number; b: boolean }>>,
+  Expect<Equal<PartialByKeys<{ a: number; b: boolean }, 'a' | 'b'>, Partial<{ a: number; b: boolean }>>>,
+]
+
+// ========== 39. Required by Keys ==========
+
+export type RequiredByKey<T extends object, K = keyof T> = Merge<
+  Pick<T, Exclude<keyof T, K>>,
+  Required<Pick<T, Extract<keyof T, K>>>
+>
+
+export type RequiredByKeyCases = [
+  Expect<Equal<RequiredByKey<{ a?: number; b?: boolean }, 'a'>, { a: number; b?: boolean }>>,
+  Expect<Equal<RequiredByKey<{ a?: number; b?: boolean }, 'a' | 'b'>, Required<{ a?: number; b?: boolean }>>>,
+]
+
+// ========== 40. Mutable ==========
+
+export type Mutable<T extends object> = {
+  -readonly [P in keyof T]: T[P]
+}
+
+export type MutableCases = [
+  Expect<Equal<Mutable<{ readonly a: number }>, { a: number }>>,
+  Expect<Equal<Mutable<{ readonly a?: number }>, { a?: number }>>,
+  Expect<Equal<Mutable<{ readonly a?: number; readonly b: boolean }>, { a?: number; b: boolean }>>,
+]
+
+// ========== 41. Omit by Type ==========
+
+export type OmitByType<T extends object, U> = {
+  [P in keyof T as T[P] extends U ? never : P]: T[P]
+}
+
+export type OmitByTypeCases = [
+  Expect<Equal<OmitByType<{ a?: 1 }, number | undefined>, {}>>,
+  Expect<Equal<OmitByType<{ a: number }, number>, {}>>,
+  Expect<Equal<OmitByType<{ a: number; b: number }, number>, {}>>,
+  Expect<Equal<OmitByType<{ a: number; b: boolean }, number>, { b: boolean }>>,
+  Expect<Equal<OmitByType<{ a: number; b: boolean }, number | boolean>, {}>>,
+]
+
+// ========== 42. ObjectEntries ==========
+
+export type ObjectEntries<T extends object, K = keyof T> = K extends keyof T ? [K, T[K]] : never
+
+export type ObjectEntriesCases = [
+  Expect<Equal<ObjectEntries<{ a: 1; b: true }>, ['a', 1] | ['b', true]>>,
+  Expect<Equal<ObjectEntries<{ a: number; b: boolean }>, ['a', number] | ['b', boolean]>>,
+  Expect<Equal<ObjectEntries<{ a?: number; b: boolean }>, ['a', number | undefined] | ['b', boolean]>>,
+  Expect<Equal<ObjectEntries<{ a: number | undefined; b: boolean }>, ['a', number | undefined] | ['b', boolean]>>,
+  Expect<Equal<ObjectEntries<{ a: number | undefined; b: boolean | null }>, ['a', number | undefined] | ['b', boolean | null]>>,
+]
+
+// ========== 43. Shift ==========
+
+export type Shift<T extends unknown[]> = T extends [unknown, ...infer R] ? R : T
+
+export type ShiftCases = [
+  // @ts-expect-error: Invalid Input
+  Shift<unknown>,
+
+  Expect<Equal<Shift<[]>, []>>,
+  Expect<Equal<Shift<[1]>, []>>,
+  Expect<Equal<Shift<[3, 2, 1]>, [2, 1]>>,
+  Expect<Equal<Shift<['a', 'b', 'c', 'd']>, ['b', 'c', 'd']>>,
+]
+
+// ========== 44. Tuple to Nested Object ==========
+
+export type TupleToNestedObject<T extends unknown[], U> = T extends [infer H, ...infer R]
+  ? { [Key in H & PropertyKey]: TupleToNestedObject<R, U> }
+  : U
+
+export type TupleToNestedObjectCases = [
+  Expect<Equal<TupleToNestedObject<['a'], string>, { a: string }>>,
+  Expect<Equal<TupleToNestedObject<['a', 'b'], number>, { a: { b: number } }>>,
+  Expect<Equal<TupleToNestedObject<['a', 'b', 'c'], boolean>, { a: { b: { c: boolean } } }>>,
+  Expect<Equal<TupleToNestedObject<[], boolean>, boolean>>,
+]
+
+// ========== 45. Reverse ==========
+
+export type Reverse<T extends unknown[]> = T extends [infer H, ...infer R] ? [...Reverse<R>, H] : T
+
+export type ReverseCases = [
+  Expect<Equal<Reverse<[]>, []>>,
+  Expect<Equal<Reverse<['a', 'b']>, ['b', 'a']>>,
+  Expect<Equal<Reverse<['a', 'b', 'c']>, ['c', 'b', 'a']>>,
+]
+
+// ========== 46. Flip Arguments ==========
+
+// export type FlipArguments<Fn extends Function> = Fn extends (...args: infer A) => infer P ? (...args: Reverse<A>) => P : never
+export type FlipArguments<T extends (...args: never[]) => unknown> = (...args: Reverse<Parameters<T>>) => ReturnType<T>
+
+export type FlipArgumentsCases = [
+  Expect<Equal<() => string, () => string>>,
+  Expect<Equal<FlipArguments<(a: number) => string>, (a: number) => string>>,
+  Expect<Equal<FlipArguments<(a: number, b: boolean) => string>, (a: boolean, b: number) => string>>,
+  Expect<Equal<FlipArguments<(a: number, b: boolean, c: string) => string>, (a: string, b: boolean, c: number) => string>>,
+]
+
+// ========== 47. FlattenDepth ==========
+
+export type FlattenDepth<T extends unknown[], D = 1, U extends unknown[] = []> = T extends [infer H, ...infer R]
+  ? H extends unknown[]
+    ? U extends { length: D }
+      ? [H, ...FlattenDepth<R, D, U>]
+      : [...FlattenDepth<H, D, [0, ...U]>, ...FlattenDepth<R, D, U>]
+    : [H, ...FlattenDepth<R, D, U>]
+  : T
+
+export type FlattenDepthCases = [
+  Expect<Equal<FlattenDepth<[]>, []>>,
+  Expect<Equal<FlattenDepth<[1, 2, 3, 4]>, [1, 2, 3, 4]>>,
+  Expect<Equal<FlattenDepth<[1, [2]]>, [1, 2]>>,
+  Expect<Equal<FlattenDepth<[1, 2, [3, 4], [[[5]]]], 2>, [1, 2, 3, 4, [5]]>>,
+  Expect<Equal<FlattenDepth<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, [[5]]]>>,
+  Expect<Equal<FlattenDepth<[1, [2, [3, [4, [5]]]]], 3>, [1, 2, 3, 4, [5]]>>,
+  Expect<Equal<FlattenDepth<[1, [2, [3, [4, [5]]]]], 19260817>, [1, 2, 3, 4, 5]>>,
 ]
